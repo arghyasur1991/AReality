a8fd81530c894f7fb59e2df9104a376c2a03b513
Arghya Sur <sur@adobe.com>
2014-06-23 22:24:53 +0530

    deleting bin folder

--- bin/AReality-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/Users/sur/Personal/Projects/AReality/bin/AReality-debug-unaligned.apk : \
-/Users/sur/Personal/Projects/AReality/bin/AReality.ap_ \
-/Users/sur/Personal/Projects/AReality/bin/classes.dex \
--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/sur/Personal/Projects/AReality/bin/AndroidManifest.xml : \
-/Users/sur/Personal/Projects/AReality/AndroidManifest.xml \
